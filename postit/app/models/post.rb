c9f72ccff24be44160d19253263f75eb164820bd
wyatt <wppurking@gmail.com>
2013-03-17 17:51:27 +0800

    finish some homework

--- postit/app/models/post.rb
+++ postit/app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ActiveRecord::Base
   attr_accessible :body, :title
+
+  belongs_to :user
+
+  has_many :comments
 end
--- postit/db/schema.rb
+++ postit/db/schema.rb
@@ -11,11 +11,25 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130310082742) do
+ActiveRecord::Schema.define(:version => 20130317094229) do
+
+  create_table "comments", :force => true do |t|
+    t.string "content"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+    t.integer "post_id"
+  end
 
   create_table "posts", :force => true do |t|
-    t.string   "title"
-    t.text     "body"
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+    t.integer "user_id"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
